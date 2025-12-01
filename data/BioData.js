3008a3d5ab0a618d80bb09286600a0402f4df88a
L3tterFake <104180634+L3tterFake@users.noreply.github.com>
2025-12-02 00:00:19 +0700

    Add files via upload

---
+++ data/BioData.js
@@ -0,0 +1,25 @@
+// date: 1 Aug, 2022
+const bioData = [
+    {
+        name: 'Dai Store One',
+        username: 'DAI STORE ONE',
+        url: '#',
+        titleImg: true,
+        avatar: '/daigital.jpg',
+        nftAvatar: true,
+        description: 'PUSAT LAPTOP SECOND ORI MURAH TERPERCAYA & BERGARANSI. ',
+        descShow: true,
+        subdesc: 'Siap Kirim Ke Seluruh Indonesia',
+        subdescShow: true,
+
+        newProduct: true,
+        author: 'Daigital',
+        authorURL: 'https://www.instagram.com/daigital_indonesia/',
+        footerText: 'made by'
+    },
+];
+
+export default bioData;
+
+
+
---
+++ data/LinksData.js
@@ -0,0 +1,62 @@
+// date 29-Jul, 2022
+const webLinks = [
+    // All social profile
+    {
+        title: 'Whatsapp',
+        url: 'https://api.whatsapp.com/send/?phone=628112345260',
+        type: 'social',
+        icon: '/whatsapp.svg',
+        on: true
+    },
+    {
+        title: 'Instagram',
+        url: 'https://instagram.com/realvjy',
+        type: 'social',
+        icon: '/insta.svg',
+        on: true
+    },
+   
+    // market section
+    {
+        title: 'Shopee',
+        url: 'https://shopee.co.id/daistore.one',
+        type: 'market',
+        icon: '/shopeee.png',
+        on: true
+    },
+    {
+        title: 'Tiktok',
+        url: 'https://tiktok.com/@daigital_indonesia',
+        type: 'market',
+        icon: '/tiktok.png',
+        on: true
+    },
+
+    {
+        title: 'Website',
+        url: '#',
+        type: 'market',
+        icon: '/web.svg',
+        on: true
+    },
+
+    {
+        title: 'Tokopedia',
+        url: 'https://www.tokopedia.com/daistoreone',
+        type: 'market',
+        icon: '/tokopedia.png',
+        on: true
+    },
+
+    {
+        title: 'maker website',
+        url: 'https://vjy.me',
+        type: 'market',
+        icon: '/vjy.png',
+        on: true
+    },
+];
+
+export default webLinks;
+
+
